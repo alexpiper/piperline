22c3734e343f1ecd71f37a32e6b0b5ab5439ab25
Alexander M Piper (DEECA) <alexander.piper@agriculture.vic.gov.au>
2024-07-26 17:45:04 +1000

    Allow multiple primers and regex matching when creating sample sheet

--- supplementary_scripts/basc_shifter.R
+++ supplementary_scripts/basc_shifter.R
@@ -1,9 +1,49 @@
-
-# commands modified from: https://github.com/jackscanlan/piperline/blob/main/jack_notes/basc_run.md
-
 # Parse input arguments 
 options <- commandArgs(trailingOnly = TRUE)
 options[options =="NA"] <- NA_character_
+
+names(options) <- c(
+  "sample_sheet",
+  "run_parameters",
+  "pcr_primers",
+  "for_primer_seq",
+  "rev_primer_seq",
+  "target_gene",
+  "max_primer_mismatch",
+  "read_min_length",
+  "read_max_length",
+  "read_max_ee",
+  "read_trunc_length",
+  "read_trim_left",
+  "read_trim_right",
+  "asv_min_length",
+  "asv_max_length",
+  "high_sensitivity",
+  "concat_unmerged",
+  "genetic_code",
+  "coding",
+  "phmm",
+  "idtaxa_db",
+  "ref_fasta",
+  "idtaxa_confidence",
+  "run_blast",
+  "blast_min_identity",
+  "blast_min_coverage",
+  "target_kingdom",
+  "target_phylum",
+  "target_class",
+  "target_order",
+  "target_family",
+  "target_genus",
+  "target_species",
+  "min_sample_reads",
+  "min_taxa_reads",
+  "min_taxa_ra",
+  "threads",
+  "start_fresh"
+)
+
+print(options)
 
 #library(renv)
 library(pak)
@@ -14,84 +54,40 @@ suppressWarnings(suppressMessages(source("_targets_packages.R")))
 suppressWarnings(suppressMessages(source("R/functions.R")))
 suppressWarnings(suppressMessages(source("R/themes.R")))
 
-# Check if all of the input params (except for threads) are empty - if so only use samplsheet and params file already provided to pipeline
-if(!all(is.na(options[1:34]))){
-  message("Creating new params file from input arguments")
-  
-  # Params to add in step_add_parameters
-  # Positions of arguments are handled by the SLURM script argument passing and should be consistent here
-  params <- tibble(
-    # Primer parameters
-    pcr_primers = options[1],
-    for_primer_seq = options[2],
-    rev_primer_seq = options[3],
-  
-    target_gene= options[4],
-    max_primer_mismatch= options[5],
-    
-    # Read filtering
-    read_min_length = options[6],
-    read_max_length = options[7],
-    read_max_ee = options[8],
-    read_trunc_length = options[9],
-    read_trim_left = options[10],
-    read_trim_right = options[11],
-    
-    # ASV filtering
-    asv_min_length = options[12],
-    asv_max_length = options[13],
-    high_sensitivity = options[14],
-    concat_unmerged = options[15],
-    genetic_code = options[16],
-    coding = options[17],
-    phmm = options[18],
-    
-    # Taxonomic assignment
-    idtaxa_db = options[19],
-    ref_fasta = options[20],
-    idtaxa_confidence = options[21],
-    run_blast= options[22],
-    blast_min_identity = options[23],
-    blast_min_coverage = options[24],
-    target_kingdom = options[25],
-    target_phylum = options[26],
-    target_class = options[27],
-    target_order = options[28],
-    target_family = options[29],
-    target_genus = options[30],
-    target_species= options[31],
-    
-    # Sample & Taxon filtering
-    min_sample_reads = options[32],
-    min_taxa_reads= options[33],
-    min_taxa_ra = options[34],
-    
-    # General pipeline parameters
-    threads = options[35]
-  )
-  
-  write_csv(params, "sample_data/loci_params.csv")
-  
-  runs <- dir("data/") #Find all directories within data
-  SampleSheet <- list.files(paste0("data/", runs), pattern= "SampleSheet", full.names = TRUE)
-  runParameters <- list.files(paste0("data/", runs), pattern= "[Rr]unParameters.xml", full.names = TRUE)
-  
-  message("Creating new sample data file from input arguments")
-  # Create samplesheet containing samples and run parameters for all runs
-  samdf <- create_samplesheet(SampleSheet = SampleSheet, runParameters = runParameters, template = "V4") %>%
-    distinct()
-  
-  # Add primers to sample sheet
-  samdf <- samdf %>%
-    mutate(pcr_primers = options[1],
-           for_primer_seq = options[2],
-           rev_primer_seq = options[3]
-    )
-} else {
-  # Read in premade samplesheet and params files
-  samdf <- read_csv("sample_data/Sample_info.csv")
-  params <- read_csv("sample_data/loci_params.csv")
+# get number of primers
+primer_length <- length(options$pcr_primers %>% str_split_1(",|;"))
+
+# Check that number of provided parameters are either 1, or match the number of provided primers
+param_lengths <- options[!names(options) %in% c("sample_sheet", "run_parameters", "pcr_primers")] %>%
+  purrr::map_dbl(~length(.x %>% str_split_1(",|;")))
+
+if(any(!param_lengths %in% c(1, primer_length))){
+  stop(names(param_lengths[param_lengths %in% c(1, primer_length)]), " parameters must have either 1 argument, or the same as pcr_primers (", primer_length,")")
 }
+
+# Get sample sheet & runparameters
+sample_sheet <- options$sample_sheet%>%
+  str_split_1(",")
+run_parameters <- options$run_parameters%>%
+  str_split_1(",")
+
+message("Creating new params file from input arguments")
+
+# Create parameters sheet
+params <- options[!names(options) %in% c("sample_sheet", "run_parameters")] %>%
+  purrr::map(~{.x %>%
+      str_split_1(",|;") %>%
+      str_remove("\\[.*\\]")}) %>%
+  bind_rows()
+
+write_csv(params, "sample_data/loci_params.csv")
+
+# Create samplesheets
+message("Creating new sample data file from input arguments")
+
+# Create samplesheet containing samples and run parameters for all runs
+samdf <- create_samplesheet(SampleSheet = sample_sheet, runParameters = run_parameters, template = "V4") %>%
+  distinct()
 
 # Check that sample_ids contain fcid, if not; attatch
 samdf <- samdf %>%
@@ -100,33 +96,57 @@ samdf <- samdf %>%
     TRUE ~ sample_id
   ))
 
-# Check that samples match samplesheet
+# Validate samples match the sample sheet
 fastqFs <- purrr::map(list.dirs("data", recursive=FALSE),
                       list.files, pattern="_R1_", full.names = TRUE) %>%
   unlist() %>%
   str_remove(pattern = "^(.*)\\/") %>%
   str_remove(pattern = "(?:.(?!_S))+$")
 
-# Filter undetermined reads from sample sheet
 fastqFs <- fastqFs[!str_detect(fastqFs, "Undetermined")]
 
-# Check for fastq files that are missing from samplesheet
-if (length(setdiff(fastqFs, samdf$sample_id)) > 0) {warning("The fastq file/s: ", setdiff(fastqFs, samdf$sample_id), " are not in the sample sheet") }
+missing_fastqFs <- setdiff(fastqFs, samdf$sample_id)
+if (length(missing_fastqFs) > 0) warning("The fastq file/s: ", missing_fastqFs, " are not in the sample sheet")
 
-# Check for sample_ids that dont have a corresponding fastq file
-if (length(setdiff(samdf$sample_id, fastqFs)) > 0) {
-  warning(paste0("The fastq file: ",
-                 setdiff(samdf$sample_id, fastqFs),
-                 " is missing, dropping from samplesheet \n")) 
-  samdf <- samdf %>%
-    filter(!sample_id %in% setdiff(samdf$sample_id, fastqFs))
+missing_sample_ids <- setdiff(samdf$sample_id, fastqFs)
+if (length(missing_sample_ids) > 0) {
+  warning("The fastq file: ", missing_sample_ids, " is missing, dropping from samplesheet \n")
+  samdf <- samdf %>% filter(!sample_id %in% missing_sample_ids)
 }
+
+# Add primer details to the sample sheet based on string matching
+
+# Function to dynamically create case_when logic for primers
+create_case_when <- function(input) {
+  case_when_expr <- map2_chr(names(input), input, ~ sprintf("str_detect(sample_name, '%s') ~ '%s'", .x, .y))
+  case_when_expr <- paste(case_when_expr, collapse = ", ")
+  case_when_expr <- paste0("case_when(", case_when_expr, ", TRUE ~ NA_character_)")
+  rlang::parse_expr(case_when_expr)
+}
+
+# Helper function to create named vectors
+create_named_vector <- function(input) {
+  input_values <- str_split_1(input, ",") %>% str_remove("\\[.*\\]")
+  input_names <- str_split_1(input, ",") %>% str_remove("].*$") %>% str_remove("^.*\\[")
+  setNames(input_values, input_names)
+}
+
+# Define named vectors for patterns and their corresponding primer sequences
+pattern_to_primers <- create_named_vector(options$pcr_primers)
+pattern_to_for <- create_named_vector(options$for_primer_seq)
+pattern_to_rev <- create_named_vector(options$rev_primer_seq)
+
+# Add primers to sample sheet based on sample_name matching
+samdf <- samdf %>%
+  mutate(pcr_primers = !!create_case_when(pattern_to_primers))%>%
+  mutate(for_primer_seq = !!create_case_when(pattern_to_for))%>%
+  mutate(rev_primer_seq = !!create_case_when(pattern_to_rev))
 
 # Write out sample tracking sheet
 write_csv(samdf, "sample_data/Sample_info.csv")
 
 # Check if start_fresh is set
-if(any(isTRUE(as.logical(options[36])))){
+if(any(isTRUE(as.logical(options$start_fresh)))){
   message("Start fresh parameter is set - removing all prior targets before running")
   tar_destroy(destroy="all", ask=FALSE)
 } else {
